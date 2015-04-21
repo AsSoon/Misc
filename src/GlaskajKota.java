0190ef2f9a8b9e5f8e6590cd724897e1bf8100a7
Anna Siewierska <absinthic@gmail.com>
2015-04-21 21:38:51 +0200

    kot

---
+++ src/GlaskajKota.java
@@ -0,0 +1,12 @@
+public class GlaskajKota {
+
+	
+	public static void main(String[] args) {
+		
+
+		
+		
+
+	}
+	
+}
---
+++ src/Kot.java
@@ -0,0 +1,42 @@
+public class Kot <String, T> {
+
+	public String imie;
+	public String kolor;
+	public T drapieznosc; // Bo kotek może wyrazić swoją drapieżnoć na wiele sposobów
+	
+	public Kot(String imie, String kolor, T drapieznosc){
+		this.imie = imie;
+		this.kolor = kolor;
+		this.drapieznosc = drapieznosc;
+	}
+	
+	 public String getImie() {
+	        return imie;
+	    }
+
+	    public void setImie(String imie) {
+	        this.imie = imie;
+	    }
+
+	    public String getKolor() {
+	        return kolor;
+	    }
+
+	    public void setKolor(String kolor) {
+	        this.kolor = kolor;
+	    }
+
+	    public T getDrapieznosc() {
+	        return drapieznosc;
+	    }
+
+	    public void setDrapieznosc(T drapieznosc) {
+	        this.drapieznosc = drapieznosc;
+	    }
+	    
+	    public void main(String[] args){
+
+	    	
+	    }
+
+}
